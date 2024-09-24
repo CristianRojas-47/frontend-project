af7f834d5879a821e3ba5a598692ea58e8307b7f
Joseph Brayan Smith Caicedo Sáenz <joseph.caice08@gmail.com>
2024-09-23 22:44:13 -0500

    feat: import registration page

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ import { Home } from "./pages/home";
 import { Contact } from "./pages/contact";
 import { Login } from "./pages/login";
 import { Details } from "./pages/details";
+import { Registration } from "./pages/Registration";
 
 const headerLinks = [
   { label: "Home", to: "/" },
@@ -27,7 +28,7 @@ const router = createBrowserRouter(
         <Route path="/" element={<Home />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/registration" element={<></>} />
+        <Route path="/registration" element={<Registration />} />
         <Route path="/comments" element={<></>} />
         <Route path="/administration" element={<></>} />
         <Route path="/movies/:movieId" element={<Details />} />
